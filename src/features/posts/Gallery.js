20b8dcf650f2f531f604519b173b22cdd44eac7b
joacopaz <joacopaz@gmail.com>
2022-11-21 22:24:37 -0300

    updated mobile responsiveness

--- src/features/posts/Gallery.js
+++ src/features/posts/Gallery.js
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import { isMobile } from "react-device-detect";
 import "./gallery.css";
 
 export function Gallery({ imgs }) {
@@ -19,24 +20,30 @@ export function Gallery({ imgs }) {
 			setAnimating(true);
 			nextRef.current.style.animationName = "enterRight";
 			activeRef.current.style.animationName = "exitLeft";
-			setTimeout(() => {
-				setActive((active) => (imgs[active + 1] ? active + 1 : 0));
-				activeRef.current.style.animationName = "unset";
-				nextRef.current.style.animationName = "unset";
-				setAnimating(false);
-			}, 190);
+			setTimeout(
+				() => {
+					setActive((active) => (imgs[active + 1] ? active + 1 : 0));
+					activeRef.current.style.animationName = "unset";
+					nextRef.current.style.animationName = "unset";
+					setAnimating(false);
+				},
+				isMobile ? 100 : 190
+			);
 		} else if (target.dataset.next !== "true" && !animating) {
 			setAnimating(true);
 			prevRef.current.style.animationName = "enterLeft";
 			activeRef.current.style.animationName = "exitRight";
-			setTimeout(() => {
-				setActive((active) =>
-					imgs[active - 1] ? active - 1 : imgs.length - 1
-				);
-				activeRef.current.style.animationName = "unset";
-				prevRef.current.style.animationName = "unset";
-				setAnimating(false);
-			}, 190);
+			setTimeout(
+				() => {
+					setActive((active) =>
+						imgs[active - 1] ? active - 1 : imgs.length - 1
+					);
+					activeRef.current.style.animationName = "unset";
+					prevRef.current.style.animationName = "unset";
+					setAnimating(false);
+				},
+				isMobile ? 100 : 190
+			);
 		}
 	};
 
@@ -64,7 +71,11 @@ export function Gallery({ imgs }) {
 						ref={activeRef}
 						onLoad={() => {
 							loaded < 3 && setLoaded((prev) => prev + 1);
-							if (adjustedHeight) return;
+							if (
+								adjustedHeight &&
+								adjustedHeight > activeRef.current.clientHeight
+							)
+								return;
 							activeRef.current.style.minHeight =
 								activeRef.current.clientHeight + "px";
 							setAdjustedHeight(true);
@@ -83,13 +94,13 @@ export function Gallery({ imgs }) {
 				</div>
 				<div
 					className="carrouselButton alignRight"
-					onClick={loaded > 2 ? handleClick : undefined}
+					onPointerDown={loaded > 2 ? handleClick : undefined}
 					data-next={true}>
 					{">"}
 				</div>
 				<div
 					className="carrouselButton alignLeft"
-					onClick={loaded > 2 ? handleClick : undefined}
+					onPointerDown={loaded > 2 ? handleClick : undefined}
 					data-next={false}>
 					{"<"}
 				</div>
