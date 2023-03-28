95a2306ee07620cada738711f2de01b27c6e5374
joacopaz <joacopaz@gmail.com>
2023-03-28 04:34:53 -0300

    Updated youtube id extraction function

--- src/features/posts/util.js
+++ src/features/posts/util.js
@@ -1,5 +1,16 @@
 import { calculateTime } from "../searchbar/searchSlice";
 
+function extractVideoId(url) {
+	let videoId = "";
+	const youtubeRegEx =
+		/^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?(?:.*&)?v=|embed\/)|youtu\.be\/)([^&\n?#]+)/;
+
+	if (url.match(youtubeRegEx)) {
+		videoId = url.match(youtubeRegEx)[1];
+	}
+
+	return videoId;
+}
 export const options = {
 	method: "GET",
 };
@@ -63,150 +74,156 @@ export const formatTime = (time) => {
 };
 
 export const handlePost = (data) => {
-	let body = data.selftext_html;
-	if (!body) {
-		body = "";
-		// console.log(data);
-	}
-	const author = `u/${data.author}`;
-	const thumbnail = data.thumbnail === "self" ? null : data.thumbnail;
-	const title = data.title;
-	const ratio = data.upvote_ratio;
-	const score = data.score;
-	const nsfw = data.over_18;
-	const flair = data.link_flair_text;
-	const linkFlairBackground = data.link_flair_background_color;
-	const linkFlairTextColor = data.link_flair_text_color;
-	const authorFlairBackground = data.author_flair_background_color;
-	const authorFlairColor = data.author_flair_text_color;
-	const authorFlair = data.author_flair_text;
-	const preview = data.preview ? data.preview.enabled : false;
-	const isTweet =
-		data.media && data.media.type && data.media.type === "twitter.com"
-			? data.media.oembed.url.match(/\d+$/)[0]
-			: false;
-	let awards = data.all_awardings.map((award) => ({
-		id: award.id,
-		icon: award.icon_url,
-		name: award.name,
-		count: award.count,
-	}));
-	if (!awards.length) awards = 0;
-	const emojis = [];
-	if (data.author_flair_richtext) {
-		data.author_flair_richtext.forEach((emoji) =>
-			emoji.u
-				? emojis.push({
-						text: emoji.u.match(/.*\/(.*)/)[1],
-						url: emoji.u,
-				  })
-				: ""
-		);
-	}
-	if (data.link_flair_richtext) {
-		data.link_flair_richtext.forEach((emoji) =>
-			emoji.u
-				? emojis.push({
-						text: emoji.u.match(/.*\/(.*)/)[1],
-						url: emoji.u,
-				  })
-				: ""
-		);
-	}
-
-	const createdAgo = calculateTime(data.created * 1000);
-	const created = new Date(data.created * 1000).toDateString();
-	const numComments = data.num_comments;
-	const id = data.id;
-	const stickied = data.stickied;
-	const commentsURL = `https://www.reddit.com/comments/${id}.json`;
-	const postHint = data.post_hint;
-	const subReddit = data.subreddit;
-	let isMedia = data.is_reddit_media_domain;
-	const sort = data.suggested_sort;
-	let video = null;
-	let gallery = null;
-	if (data.is_gallery) {
-		for (const img in data.media_metadata) {
-			const media = data.media_metadata[img];
-			const format = media.m?.match(/\/(.*)/)[1];
-			const url = `https://i.redd.it/${media.id}.${format}`;
-			isMedia = "gallery";
-			if (!gallery) gallery = [];
-			gallery.push(url);
+	try {
+		let body = data.selftext_html;
+		if (!body) {
+			body = "";
+			// console.log(data);
 		}
-	}
-
-	let img = null;
-	if (isMedia) {
-		if (data.is_video) {
-			video = {
-				videoURL: data.media.reddit_video.fallback_url.match(
-					/(.*)\?source=fallback/
-				)[1],
-				audioURL: (
-					data.media.reddit_video.fallback_url.match(/(.*)DASH_/)[1] +
-					"DASH_audio." +
-					data.media.reddit_video.fallback_url.match(/\.([^.]*)$/)[1]
-				).match(/(.*)\?source=fallback/)[1],
-			};
-			isMedia = "video";
+		const author = `u/${data.author}`;
+		const thumbnail = data.thumbnail === "self" ? null : data.thumbnail;
+		const title = data.title;
+		const ratio = data.upvote_ratio;
+		const score = data.score;
+		const nsfw = data.over_18;
+		const flair = data.link_flair_text;
+		const linkFlairBackground = data.link_flair_background_color;
+		const linkFlairTextColor = data.link_flair_text_color;
+		const authorFlairBackground = data.author_flair_background_color;
+		const authorFlairColor = data.author_flair_text_color;
+		const authorFlair = data.author_flair_text;
+		const preview = data.preview ? data.preview.enabled : false;
+		const isTweet =
+			data.media && data.media.type && data.media.type === "twitter.com"
+				? data.media.oembed.url.match(/\d+$/)[0]
+				: false;
+		let awards = data.all_awardings.map((award) => ({
+			id: award.id,
+			icon: award.icon_url,
+			name: award.name,
+			count: award.count,
+		}));
+		if (!awards.length) awards = 0;
+		const emojis = [];
+		if (data.author_flair_richtext) {
+			data.author_flair_richtext.forEach((emoji) =>
+				emoji.u
+					? emojis.push({
+							text: emoji.u.match(/.*\/(.*)/)[1],
+							url: emoji.u,
+					  })
+					: ""
+			);
 		}
-		if (!data.is_video && !data.is_gallery) {
+		if (data.link_flair_richtext) {
+			data.link_flair_richtext.forEach((emoji) =>
+				emoji.u
+					? emojis.push({
+							text: emoji.u.match(/.*\/(.*)/)[1],
+							url: emoji.u,
+					  })
+					: ""
+			);
+		}
+
+		const createdAgo = calculateTime(data.created * 1000);
+		const created = new Date(data.created * 1000).toDateString();
+		const numComments = data.num_comments;
+		const id = data.id;
+		const stickied = data.stickied;
+		const commentsURL = `https://www.reddit.com/comments/${id}.json`;
+		const postHint = data.post_hint;
+		const subReddit = data.subreddit;
+		let isMedia = data.is_reddit_media_domain;
+		const sort = data.suggested_sort;
+		let video = null;
+		let gallery = null;
+		if (data.is_gallery) {
+			for (const img in data.media_metadata) {
+				const media = data.media_metadata[img];
+				const format = media.m?.match(/\/(.*)/)[1];
+				const url = `https://i.redd.it/${media.id}.${format}`;
+				isMedia = "gallery";
+				if (!gallery) gallery = [];
+				gallery.push(url);
+			}
+		}
+
+		let img = null;
+		if (isMedia) {
+			if (data.is_video) {
+				video = {
+					videoURL: data.media.reddit_video.fallback_url.match(
+						/(.*)\?source=fallback/
+					)[1],
+					audioURL: (
+						data.media.reddit_video.fallback_url.match(/(.*)DASH_/)[1] +
+						"DASH_audio." +
+						data.media.reddit_video.fallback_url.match(/\.([^.]*)$/)[1]
+					).match(/(.*)\?source=fallback/)[1],
+				};
+				isMedia = "video";
+			}
+			if (!data.is_video && !data.is_gallery) {
+				img = data.url;
+			}
+		}
+		if (postHint === "image") {
+			isMedia = "image";
 			img = data.url;
 		}
+		if (!isMedia) isMedia = "text";
+		let link;
+		if (isMedia === "text" && data.url_overridden_by_dest) isMedia = "link";
+		let isYoutube;
+		if (isMedia === "link") {
+			link = data.url_overridden_by_dest;
+			if (data.domain.match(/youtu/)) {
+				isYoutube = extractVideoId(link);
+			}
+		}
+		if (isMedia === "text" && body.match(/https:\/\/preview.redd.it/)) {
+			isMedia = "image";
+			img = `https://i.redd.it/${
+				body.match(/https:\/\/preview.redd.it\/(.+)\?/)[1]
+			}`;
+		}
+
+		return {
+			author,
+			linkFlairBackground,
+			linkFlairTextColor,
+			thumbnail,
+			title,
+			stickied,
+			ratio,
+			score,
+			awards,
+			flair,
+			authorFlair,
+			authorFlairBackground,
+			authorFlairColor,
+			createdAgo,
+			created,
+			numComments,
+			id,
+			commentsURL,
+			isMedia,
+			video,
+			gallery,
+			img,
+			body,
+			emojis,
+			preview,
+			isTweet,
+			link,
+			isYoutube,
+			subReddit,
+			sort,
+		};
+	} catch (error) {
+		console.log(error);
 	}
-	if (postHint === "image") {
-		isMedia = "image";
-		img = data.url;
-	}
-	if (!isMedia) isMedia = "text";
-	let link;
-	if (isMedia === "text" && data.url_overridden_by_dest) isMedia = "link";
-	let isYoutube;
-	if (isMedia === "link") {
-		link = data.url_overridden_by_dest;
-		if (data.domain.match(/youtu/))
-			isYoutube = data.url_overridden_by_dest.match(/(\w+)$/)[0];
-	}
-	if (isMedia === "text" && body.match(/https:\/\/preview.redd.it/)) {
-		isMedia = "image";
-		img = `https://i.redd.it/${
-			body.match(/https:\/\/preview.redd.it\/(.+)\?/)[1]
-		}`;
-	}
-	return {
-		author,
-		linkFlairBackground,
-		linkFlairTextColor,
-		thumbnail,
-		title,
-		stickied,
-		ratio,
-		score,
-		awards,
-		flair,
-		authorFlair,
-		authorFlairBackground,
-		authorFlairColor,
-		createdAgo,
-		created,
-		numComments,
-		id,
-		commentsURL,
-		isMedia,
-		video,
-		gallery,
-		img,
-		body,
-		emojis,
-		preview,
-		isTweet,
-		link,
-		isYoutube,
-		subReddit,
-		sort,
-	};
 };
 
 export const handleComment = (data, kind) => {
